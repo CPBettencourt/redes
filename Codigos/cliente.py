20a39371f15653cca19929125e85ef88335273f7
JPMacabu <57292285+JPMacabu@users.noreply.github.com>
2019-11-02 13:20:11 -0300

    cliente.py completo (echo)

--- Codigos/cliente.py
+++ Codigos/cliente.py
@@ -1,3 +1,27 @@
 #----------Trabalho de Redes----------
 #comunicação clinte servidor
 #codigo para o cliente
+
+#Os comentários aqui adicionados são para dar início ao relatório
+
+import socket
+
+host = ''
+port = 8080
+addr = (host, port)
+
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Criação do mecanismo de socket para estabelecimento de conexão; AF_INET indica uso do
+#protocolo IPv4, SOCK_STREAM indica uso do protocolo TCP
+
+conn.connect(addr)
+#Estabelecimento da conexão ao servidor através do host e porta pré-definidos
+
+conn.send("Hello, me.")
+#Envio de dados ao servidor
+
+data = conn.recv(4096)
+#Recebimento de dado do servidor de até 4096 bytes
+
+conn.close()
+print data
