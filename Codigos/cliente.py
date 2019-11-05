1a99ba4ac8c02492471634e80ce690545c01cbcc
JPMacabu <57292285+JPMacabu@users.noreply.github.com>
2019-11-04 21:58:53 -0300

    Update cliente.py

--- Codigos/cliente.py
+++ Codigos/cliente.py
@@ -17,8 +17,9 @@ conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 conn.connect(addr)
 #Estabelecimento da conexão ao servidor através do host e porta pré-definidos
 
-conn.send("Hello, me.")
-#Envio de dados ao servidor
+line = input()
+conn.send(line)
+#Entrada de dados e envio ao servidor
 
 data = conn.recv(4096)
 #Recebimento de dado do servidor de até 4096 bytes
