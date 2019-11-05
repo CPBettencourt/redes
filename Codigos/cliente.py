e440ab8e445112b15978d77a3ce02531ef8aa0dc
JPMacabu <57292285+JPMacabu@users.noreply.github.com>
2019-11-04 22:16:00 -0300

    Update cliente.py

--- Codigos/cliente.py
+++ Codigos/cliente.py
@@ -17,12 +17,15 @@ conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 conn.connect(addr)
 #Estabelecimento da conexão ao servidor através do host e porta pré-definidos
 
-line = input()
-conn.send(line)
-#Entrada de dados e envio ao servidor
-
-data = conn.recv(4096)
-#Recebimento de dado do servidor de até 4096 bytes
+while True:
+	line = input()
+	conn.send(line)
+	#Entrada de dados e envio ao servidor
+	
+	data = conn.recv(4096)
+	#Recebimento de dado do servidor de até 4096 bytes
+	if not data:
+		break
+	print data
 
 conn.close()
-print data
