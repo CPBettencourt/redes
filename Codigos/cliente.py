c0991d72f70179673f35aec72fabfb11cd0fc5ec
clarabette <clarabette@poli.ufrj.br>
2019-11-05 14:14:41 -0300

    Remoção de acentos

--- Codigos/cliente.py
+++ Codigos/cliente.py
@@ -1,20 +1,20 @@
-#comunicação clinte servidor
+#comunicacao clinte servidor
 #codigo para o cliente
 
-#Os comentários aqui adicionados são para dar início ao relatório
+#Os comentarios aqui adicionados sao para dar inicio ao relatorio
 
 import socket
 
-host = ''
-port = 2000
-addr = (host, port)
+host = '192.168.10'
+port = 1234
+addr = (host, port) 
 
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Criação do mecanismo de socket para estabelecimento de conexão; AF_INET indica uso do
+#Criacao do mecanismo de socket para estabelecimento de conexao; AF_INET indica uso do
 #protocolo IPv4, SOCK_STREAM indica uso do protocolo TCP
 
 conn.connect(addr)
-#Estabelecimento da conexão ao servidor através do host e porta pré-definidos
+#Estabelecimento da conexao ao servidor atraves do host e porta pre-definidos
 
 while True:
 	line = input()
@@ -22,7 +22,7 @@ while True:
 	#Entrada de dados e envio ao servidor
 	
 	data = conn.recv(4096)
-	#Recebimento de dado do servidor de até 4096 bytes
+	#Recebimento de dado do servidor de ate 4096 bytes
 	if not data:
 		break
 	print (data)
