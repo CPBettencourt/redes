be43eaeb2066fac3c205fba6c24bf2db1732a02a
Gabriela Alcantara <gabii.a.alcantara@poli.ufrj.br>
2019-11-18 14:49:01 -0300

    Update peer_to_peer.py

--- Codigos/peer_to_peer.py
+++ Codigos/peer_to_peer.py
@@ -11,7 +11,7 @@ tam_cabe = 5
 def udp_chat():
     global nome 
     global transm_socket
-    global atual
+    global atualmente_conectado
 
     while True:
         #Le o caractere que indica o tipo de mensagem
@@ -43,7 +43,7 @@ def udp_chat():
             
         #Se for um aviso de saida da rede
         elif usuario[:1] == 's':
-            if (usuar[1:] in atualmente_conectado):
+            if (usuario[1:] in atualmente_conectado):
                 atualmente_conectado.remove(usuario[1:])
                 print("Conexao encerrada com " + usuario[1:])
                 print("Total de usuarios conectados: " + str(len(atualmente_conectado)))
@@ -65,4 +65,65 @@ def envia_mensagem_trasmissao():
             os._exit(1)
         
         #Se a mensagem nao estiver vazia
-        elif
+        elif data != '':
+            usuario = ('m'+nome).encode('utf-8')
+            cabe_usuario = f"{len(usuario):<{tam_cabe}}".encode('utf-8')
+            mensagem = data.encode('utf-8')
+            cabe_mensagem = f"{len(mensagem):<{tam_cabe}}".encode('utf-8')
+            enviar_socket.sendto(cabe_usuario+usuario+cabe_mensagem+mensagem, ('255.255.255.255', 2000))
+
+#Mantem o envio do nome do usuario para a rede
+def envia_status_trasmissao():
+    global nome
+    global enviar_socket
+    enviar_socket.setblocking(False)
+    usuario = ('o'+nome).encode('utf-8')
+    cabe_usuario = f"{len(usuario):<{tam_cabe}}".encode('utf-8')
+    while True:
+        time.sleep(1)
+        enviar_socket.sendto(cabe_usuario+usuario, ('255.255.255.255', 2000))
+
+def main():
+    global transm_socket
+
+    transm_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)      
+    transm_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   
+    transm_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)   
+    transm_socket.bind(('0.0.0.0', 2000))
+
+    global enviar_socket
+
+    enviar_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)           
+    enviar_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+
+    print("Bem-vindo ao Chat P2P \nPara sair digite: Exit()")
+
+    #Escolha do nome de usuario
+    global nome
+    nome = ''
+    while True:
+        if not nome:
+            nome = input('Usuario: ')
+            if not nome:
+                print('Insira um nome de usuario valido')
+            else:
+                break
+    print('*************************************************')  
+
+    global recebe_thread
+    recebe_thread = Thread(target=udp_chat)               
+    global enviar_msg_thread
+    enviar_msg_thread = Thread(target=envia_mensagem_trasmissao)  
+    global atualmente_conectado
+    atualmente_conectado = []                                         
+    global enviar_online_thread
+    enviar_online_thread = Thread(target=envia_status_trasmissao) 
+    recebe_thread.start()                                          
+    enviar_msg_thread.start()                                       
+    enviar_online_thread.start()                                    
+    recebe_thread.join()                                           
+    enviar_msg_thread.join()                                        
+    enviar_online_thread.join()                                     
+
+if __name__ == '__main__':
+    main()
